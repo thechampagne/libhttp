3f131a7f1469757759f35971f7e1323a35c25667
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-01-09 04:27:56 +0200

    Add http_post function

--- main.d
+++ main.d
@@ -46,3 +46,56 @@ extern(C) http_t http_get(const char* url,
     return http;
   }
 }
+
+extern(C) http_t http_post(const char* url,
+			   const char**  args_keys,
+			   const char**  args_values,
+			   size_t args_len,
+			   const char** cookies_keys,
+			   const char** cookies_values,
+			   size_t cookies_len)
+{
+  string[string] cookies;
+  string[string] args;
+
+  if (args_keys == null || args_values == null)
+  {
+    args = null;
+  }
+  else
+  {
+    for (size_t i = 0; i < args_len; i++)
+    {
+      args[args_keys[i].fromStringz.idup] = args_values[i].fromStringz.idup;
+    }
+  }
+  if (cookies_keys == null || cookies_values == null)
+  {
+    cookies = null;
+  }
+  else
+  {
+    for (size_t i = 0; i < cookies_len; i++)
+    {
+      cookies[cookies_keys[i].fromStringz.idup] = cookies_values[i].fromStringz.idup;
+    }
+  }
+  http_t http;
+  try
+  {
+    auto str = post(url.fromStringz.idup, args,cookies);
+    auto res = cast(char*) malloc(char.sizeof * str.length + 1);
+    strcpy(res, str.toStringz);
+    http.buffer = res;
+    http.is_err = 0;
+    return http;
+  }
+  catch(Exception ex)
+  {
+    auto res = cast(char*) malloc(char.sizeof * ex.msg.length + 1);
+    strcpy(res, ex.msg.toStringz);
+    http.buffer = res;
+    http.is_err = 1;
+    return http;
+  }  
+}

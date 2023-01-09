f9c47bffff8f2985ded19d8d265dc1167722114b
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-01-09 05:44:59 +0200

    Add http_get_binary function with custom type http_binary_t

--- main.d
+++ main.d
@@ -10,10 +10,17 @@ struct http_t
   char* buffer;
 }
 
+struct http_binary_t
+{
+  int is_err;
+  size_t buffer_len;
+  ubyte* buffer;
+}
+
 extern(C) http_t http_get(const char* url,
-		   const char** cookies_keys,
-		   const char** cookies_values,
-		   size_t cookies_len)
+			  const char** cookies_keys,
+			  const char** cookies_values,
+			  size_t cookies_len)
 {
   string[string] cookies;
   if (cookies_keys == null || cookies_values == null)
@@ -98,4 +105,44 @@ extern(C) http_t http_post(const char* url,
     http.is_err = 1;
     return http;
   }  
+}
+
+extern(C) http_binary_t http_get_binary(const char* url,
+				  const char** cookies_keys,
+				  const char** cookies_values,
+				  size_t cookies_len)
+{
+  string[string] cookies;
+  if (cookies_keys == null || cookies_values == null)
+  {
+    cookies = null;
+  }
+  else
+  {
+    for (size_t i = 0; i < cookies_len; i++)
+    {
+      cookies[cookies_keys[i].fromStringz.idup] = cookies_values[i].fromStringz.idup;
+    }
+  }
+  http_binary_t http;
+  try
+  {
+    auto bin = getBinary(url.fromStringz.idup, cookies);
+    auto res = cast(ubyte*) malloc(ubyte.sizeof * bin.length);
+    memcpy(res, cast(void*) bin, bin.length);
+    http.buffer = res;
+    http.buffer_len = bin.length;
+    http.is_err = 0;
+    return http;
+  }
+  catch(Exception ex)
+  {
+    auto res = cast(ubyte*) malloc(ubyte.sizeof * ex.msg.length + 1);
+    memcpy(res, ex.msg.toStringz, ex.msg.length);
+    res[ex.msg.length] = '\0';
+    http.buffer = res;
+    http.buffer_len = ex.msg.length;
+    http.is_err = 1;
+    return http;
+  }
 }
